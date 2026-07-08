c86c66f89091a2ff0c14200f0ffe773bd4ec817c
jjmolomo17-tech <jjmolomo17@gmail.com>
2026-07-08 21:46:16 +0200

    Home.tsx editted

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,13 @@
+function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold mb-4">Home Page</h1>
+      <p className="text-lg text-gray-700">Welcome to the Home page!</p>
+    </div>
+  );
+}
+
+export default Home;
 
 
 
