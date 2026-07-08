34a7b5706549429b2371177bda156c08da9ca528
jjmolomo17-tech <jjmolomo17@gmail.com>
2026-07-08 21:48:05 +0200

    Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,9 +2,7 @@ function Contact() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-4xl font-bold mb-4">Contact Page</h1>
-      <p className="text-lg text-gray-700">
-        Feel free to reach out to us anytime!
-      </p>
+      <p className="text-lg text-gray-700">Feel free to reach out!</p>
     </div>
   );
 }
