738fa1ef5dc4745e441735627b1cf38b60baad14
jjmolomo17-tech <jjmolomo17@gmail.com>
2026-07-08 17:57:36 +0200

    app.tsx browserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
 import Student from "./components/student";
 
 function App() {
   return (
     <div>
-      <h1>Student Information</h1>
-      <Student name="John Doe" age={20} course="Computer Science" />
+      <BrowserRouter>
+
+
+     <Student name="John Doe" age={20} course="Computer Science" /> 
+      </BrowserRouter>
     </div>
   );
 }
