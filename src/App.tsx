d829fc6b44c551bbaf9b9fc61021d0fb1294c283
jjmolomo17-tech <jjmolomo17@gmail.com>
2026-07-08 21:42:06 +0200

    App.tsx imports corrected

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import { Routes, Route } from "react-router-dom";
-
-
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-     <Route path="/about" element={<About/>}></Route>
-     <Route path="/contact" element={<Contact/>}></Route>
-     <Route path="/home" element={<Home/>}></Route>
-         </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
