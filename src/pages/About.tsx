68939d55608d4a935c6c66ca58e83b08bbdb6de0
jjmolomo17-tech <jjmolomo17@gmail.com>
2026-07-08 20:02:00 +0200

    pages files created

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+function About() {
+   return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1 className="text-4xl font-bold mb-4">About Page</h1>
+        <p className="text-1g text-gray-700">
+         This is the about page of our application. Here you can find information about the project    
+        </p>
+    </div>
+   ) 
+}
