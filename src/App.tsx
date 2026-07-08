504209b1890ed1c6a759dd81af3f1b814313c1c2
jjmolomo17-tech <jjmolomo17@gmail.com>
2026-07-08 21:04:38 +0200

    Student.tsc problem fixed

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Contact from "./pages/Contact";
 import { Routes, Route } from "react-router-dom";
 
 
+
 function App() {
   return (
     <div>
--- src/components/student.tsx
+++ src/components/student.tsx
@@ -1,14 +1,19 @@
+import React from "react";
+
 interface StudentProps {
-    name: string;
-    age: number;
-    course: string
+  name: string;
+  age: number;
+  course: string;
 }
 
-function Student({ name, age, course} StudentProps) {
-    return (
-        <div>
-        <h2>Name: {name}</h2>
-        <p>Course: {course}</p>
-        </div>
-    )
-}
+const Student: React.FC<StudentProps> = ({ name, age, course }) => {
+  return (
+    <div>
+      <h2>Name: {name}</h2>
+      <p>Age: {age}</p>
+      <p>Course: {course}</p>
+    </div>
+  );
+};
+
+export default Student;
