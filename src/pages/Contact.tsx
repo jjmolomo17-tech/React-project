a9062b467bf36b229f0e3e23321f4d5191c10da4
jjmolomo17-tech <jjmolomo17@gmail.com>
2026-07-08 20:12:15 +0200

    Contact.tsx

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,10 @@
+function Contact() {
+    return {
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+             <h1 className="text-4xl font-bold mb-4">Contact Page</h1>
+             <p className="text-1g text-gray-700">
+             </p>
+    }
+}
+
+export default Contact;
