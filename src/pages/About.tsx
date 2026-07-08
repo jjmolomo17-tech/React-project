3db99529e74b4c8d75fde24c75ce0c0dfa77eb7c
jjmolomo17-tech <jjmolomo17@gmail.com>
2026-07-08 21:14:36 +0200

    About.tsx problem fixed

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,12 @@
 function About() {
-   return (
+  return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1 className="text-4xl font-bold mb-4">About Page</h1>
-        <p className="text-1g text-gray-700">
-         This is the about page of our application. Here you can find information about the project    
-        </p>
+      <h1 className="text-4xl font-bold mb-4">About Page</h1>
+      <p className="text-lg text-gray-700">
+        This is the about page of our application. Here you can find information about the project.
+      </p>
     </div>
-   ) 
+  );
 }
+
+export default About;
