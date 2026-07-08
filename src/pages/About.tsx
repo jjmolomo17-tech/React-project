abbda474111294172d97d7a88ad39ef57bb50131
jjmolomo17-tech <jjmolomo17@gmail.com>
2026-07-08 21:47:04 +0200

    About.tsx edittted

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,9 +2,7 @@ function About() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-4xl font-bold mb-4">About Page</h1>
-      <p className="text-lg text-gray-700">
-        This is the about page of our application. Here you can find information about the project.
-      </p>
+      <p className="text-lg text-gray-700">This is the About page.</p>
     </div>
   );
 }
